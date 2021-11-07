83228168354c45fb9a47300c8086e384848dbad0
arunmishra2000 <arunkumar.mishra2019@vitstudent.ac.in>
2021-11-07 14:00:28 +0530

    changes

---
+++ posts/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+const { randomBytes } = require("crypto");
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex"); //4 bytes
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
